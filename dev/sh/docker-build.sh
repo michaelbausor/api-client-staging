a19e9a11ffd93190dedf227db9c368ace160e53d
Michael Bausor <michaelbausor@google.com>
2018-06-14 16:28:00 -0700

    fix image name

--- dev/sh/docker-build.sh
+++ dev/sh/docker-build.sh
@@ -4,5 +4,5 @@ set -e
 
 DATETAG=$(date '+%Y%m%d-%H%M%S')
 docker pull googleapis/artman:latest
-docker build --no-cache -t googleapis/artman-autobuild:latest .
-docker tag googleapis/artman-autobuild:latest googleapis/artman-autobuild:$DATETAG
+docker build --no-cache -t googleapis/artman:autobuild-latest .
+docker tag googleapis/artman:autobuild-latest googleapis/artman:autobuild-$DATETAG
